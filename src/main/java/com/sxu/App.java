420812f8a4410d755eaa5e179556b5875836dc2c
1303575952@qq.com <1303575952@qq.com>
2019-01-05 14:12:28 +0800

    main中使用线程池，三个线程：污染物溯源、空气质量预报、天气预报。

--- src/main/java/com/sxu/App.java
+++ src/main/java/com/sxu/App.java
@@ -6,12 +6,31 @@ import com.sxu.timetask.WeatherForecastPost;
 import com.sxu.util.DateAndTimeUtil;
 
 import java.util.Timer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
     public static void main(String[] args) {
-        Timer timer = new Timer();
-        //timer.schedule(new TraceSourcePost(),DateAndTimeUtil.getPostTime(),86400000);
-        //timer.schedule(new AirQualityForecastPost(),DateAndTimeUtil.getPostTime(),86400000);
-        timer.schedule(new WeatherForecastPost(),DateAndTimeUtil.getPostTime(),86400000);
+        final Timer timer = new Timer();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                timer.schedule(new TraceSourcePost(),DateAndTimeUtil.getPostTime(),86400000);
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                timer.schedule(new AirQualityForecastPost(),DateAndTimeUtil.getPostTime(),86400000);
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                timer.schedule(new WeatherForecastPost(),DateAndTimeUtil.getPostTime(),86400000);
+            }
+        });
+
     }
 }
